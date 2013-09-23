738efc4c1553cc2e47060f8c5a1d65c39b95f589
zflies <zachflies11@gmail.com>
2013-09-23 14:44:05 -0500

    Updated UI

--- BACSafe/src/com/example/bacsafe/Main.java
+++ BACSafe/src/com/example/bacsafe/Main.java
@@ -2,6 +2,7 @@ package com.example.bacsafe;
 
 import android.app.TabActivity;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 
@@ -11,28 +12,36 @@ public class Main extends TabActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
 
         TabHost tabHost=getTabHost();
         tabHost.setup();
 
+        //Tab - Drink Counter
         TabSpec spec1=tabHost.newTabSpec("Drink Counter");
-        spec1.setContent(R.id.tabDrinkCounter);
         spec1.setIndicator("Drink Counter");
-
+        spec1.setContent(R.id.tabDrinkCounter);
+     
+        //Tab - Night Group
         TabSpec spec2=tabHost.newTabSpec("Night Group");
         spec2.setIndicator("Night Group");
-        spec2.setContent(R.id.tab2);
+        spec2.setContent(R.id.tabNightGroup);
 
+        //Tab - Friends List
         TabSpec spec3=tabHost.newTabSpec("Friends List");
         spec3.setIndicator("Friends List");
-        spec3.setContent(R.id.tab3);
+        spec3.setContent(R.id.tabFriendsList);
 
+       
         tabHost.addTab(spec1);
         tabHost.addTab(spec2);
         tabHost.addTab(spec3);
     }
-
-
 }
