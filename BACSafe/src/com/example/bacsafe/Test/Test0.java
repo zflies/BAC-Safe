656b2066b649a05fbe6ae1656a5ec6a9407ea5cc
acwhite211 <acwhite2011@gmail.com>
2013-12-06 11:05:45 -0600

    shannon's junit code

--- BACSafe/src/com/example/bacsafe/Test/Test0.java
+++ BACSafe/src/com/example/bacsafe/Test/Test0.java
@@ -20,55 +20,20 @@ import com.example.bacsafe.*;
 
 public class Test0 {
 
-	//backend tests
-	//adding user
-	/*@Test
-	public void backuser() {
-		assertEquals(true, TestAddUser());
-	}*/
-
-	//adding buddy
-	/*@Test
-	public void backbuddy() throws InterruptedException, ExecutionException{
-		BacsafeAPI.Builder builder = new BacsafeAPI.Builder(AndroidHttp.newCompatibleTransport(), new GsonFactory(), null);
-		BacsafeAPI service = builder.build();
-		String log = "";
-		try {
-			log = service.userinfo().createBuddies(info[0]).execute().getMessage();
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-
-		assertEquals(true, log.compareTo("Success") == 0);
-	}
-
-	//adding group
-	@Test
-	public void backgroup() throws InterruptedException, ExecutionException{
-		Group group = new Group("TestGroup");
-		group.addGroupMember(new Buddy("TestUser"));
-		group.addGroupMember(new Buddy("TestUserBuddy"));
-		createGroup(group);
-		LinkedList<String> members = new LinkedList<String>();
-		members.add("TestUser");
-		members.add("TestUserBuddy");
-		
-		assertEquals(members, getGroupDrinkers("TestGroup"));
-	}*/
-
-
-
 	//main tests
+	//BAC
 	@Test
 	public void BAC() {
 		assertEquals(.10, TestgenerateBAC(1, 2, 0, true, 130, 1), .01);
 	}
 	
+	//BAC Timer
 	@Test
 	public void timer(){
-		assertEquals(496, TestgenerateBACTimer(.1), .1);
+		assertEquals(500, TestgenerateBACTimer(.1), 1);
 	}
 
+	//Number of Drinks
 	@Test
 	public void number(){
 		//Main tester = new Main();
@@ -76,7 +41,92 @@ public class Test0 {
 
 	}
 	
+	//Reset values
+	@Test
+	public void reset(){
+		assertEquals(0, resetAllDrinkValues(2, 1, 0, 1, .1), .1);
+	}
+	
+	//Beer
+	@Test
+	public void beer(){
+		assertEquals(4, generateBeerDrinks(4), 0);
+	}
+	//Wine
+	@Test
+	public void wine(){
+		assertEquals(2, generateWineDrinks(2), 0);
+	}
+	//Shot
+	@Test
+	public void shot(){
+		assertEquals(3, generateBeerDrinks(3),0);
+			
+	}
 
+	//user tests -- all the information in the profile user cases, (both initial and editing)	
+	@Test
+	public void Age(){
+		setAge(30);
+		assertEquals(30, getAge(), 0);
+	}
+
+	@Test
+	public void FirstName(){
+		setFirstName("Name");
+		assertEquals("Name", getFirstName());
+	}
+
+	@Test
+	public void HeightFeet(){
+		setHeightFeet(6);
+		assertEquals(6, getHeightFeet(), 0);
+	}
+
+	@Test
+	public void HeighInches(){
+		setHeightInches(4);
+		assertEquals(4, getHeightInches(),0);
+	}
+
+	@Test
+	public void LastName(){
+		setLastName("Name");
+		assertEquals("Name", getLastName());
+	}
+
+	@Test
+	public void Weight(){
+		setWeight(150);
+		assertEquals(150, getWeight(),0);
+	}
+
+	@Test
+	public void yesMale(){
+		setMale(true);
+		assertEquals(true, isMale());
+	}
+	
+	//backend/integration
+	@Test
+	public void UserName(){
+		setUserName("Name");
+		assertEquals("Name", getUserName());
+	}
+	
+	@Test
+	public void buddy(){
+		setBuddy("buddy");
+		assertEquals(true, getBuddy("buddy"));
+		
+	}
+	
+	@Test
+	public void group(){
+		setGroup("group");
+		assertEquals(true, getGroup("group"));
+	}
+ 
 	
 	
 	
@@ -88,17 +138,13 @@ public class Test0 {
 		return nDrinkTotal;
 	}
 
-	//Testing classes
 	public double TestgenerateBAC(int beer, int wine, int shot, boolean isMale, int weight, int nCurrentDrink){
-
-		//TestgenerateNumberDrinks(beer, wine, shot);
 
 		return generateBAC(isMale, weight, nCurrentDrink,TestgenerateNumberDrinks(beer, wine, shot) );
 
 	}
 	
 	public double TestgenerateBACTimer(double bac){
-
 		int m_nBACtimerMinute = 0;
 
 		double i = bac;
@@ -110,9 +156,6 @@ public class Test0 {
 		return m_nBACtimerMinute++;
 		}
 
-	/**
-	 * Estimates the User's current BAC percentage
-	 */
 	private double generateBAC(boolean isMale, int weight, int nCurrentDrink, int nNumDrinks){
 
 		//Initialize Variables
@@ -143,110 +186,118 @@ public class Test0 {
 		return m_dBACpercent;
 	} // generateBAC()
 
+	private double resetAllDrinkValues(int beer, int wine, int shot, int current, double BACpercent){
+		beer = 0;
+		wine = 0; 
+		shot = 0; 
+		current = 0;
+		BACpercent = 0;
 
-
-	/*
-		@Test
-		public void reset(){
-			Main tester = new Main();
-			assertEquals(0, tester.TestresetAllDrinkValues(), .1);
-		}
-	 */
-	/*
-		//user tests	
-		@Test
-		public void Age() throws InterruptedException, ExecutionException{
-			User tester = new User();
-			try {
-				assertEquals(21, tester.TestAge(21), 0);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Test
-		public void FirstName() throws InterruptedException, ExecutionException{
-			User tester = new User();
-			try {
-				assertEquals("Name", tester.TestFName("Name"));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Test
-		public void HeightFeet() throws InterruptedException, ExecutionException{
-			User tester = new User();
-			try {
-				assertEquals(6, tester.TestHeight(6), 0);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Test
-		public void HeighInches() throws InterruptedException, ExecutionException{
-			User tester = new User();
-			try {
-				assertEquals(0, tester.TestInches(0));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Test
-		public void LastName() throws InterruptedException, ExecutionException{
-			User tester = new User();
-			try {
-				assertEquals("Name", tester.TestLName("Name"));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Test
-		public void UserName() throws InterruptedException, ExecutionException{
-			User tester = new User();
-			try {
-				assertEquals("Name", tester.Testusername("Name"));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Test
-		public void Weight() throws InterruptedException, ExecutionException{
-			User tester = new User();
-			try {
-				assertEquals(130, tester.TestWeight(130), 0);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Test
-		public void isMale() throws InterruptedException, ExecutionException{
-			User tester = new User();
-			try {
-				assertEquals(true, tester.TestisMale(true));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	 */
+		return (beer+wine+shot+current+BACpercent);
+	}
 	
-	//Tests Backend
-	/*public boolean TestAddUser(){
-		String log = "";
-		try {
-			log = userAccountSetup("testUserName", "Tester", "McTester");
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			e.printStackTrace();
-		}
-		if(log.compareTo("Success") == 0) {
-			return true;
-		}else return false;
-	}*/
+	private int generateBeerDrinks(int num){
+		return num;
+	} // generateBeerDrinks
+	
+	private int generateWineDrinks(int num){
+		return num;
+	} // generateBeerDrinks
+	
+	private int generateShotDrinks(int num){
+		return num;
+	} // generateBeerDrinks
+	
+	int nage;
+	protected int getAge() {
+		return nage;
+	} // getAge()
+
+	protected void setAge(int nAge) {
+		nage = nAge;
+	} // setAge()
+	
+	String fname;
+	protected String getFirstName() {
+		return fname;
+	} // getFirsttName()
+
+	protected void setFirstName(String sFirstName) {
+
+		//TODO: Save user's first name to database
+
+		fname = sFirstName;
+	} // setFirstName()
+	
+	int feet;
+	protected int getHeightFeet() {
+		return feet;
+	} // getHeightFeet()
+
+	protected void setHeightFeet(int nHeightFeet) {
+		feet = nHeightFeet;
+	} // setHeightFeet()
+	
+	int inch;
+	protected int getHeightInches() {
+		return inch;
+	} // getHeightInches()
+
+	protected void setHeightInches(int nHeightInches) {
+		inch = nHeightInches;
+	} // setHeightInches()
+	
+	String lname;
+	protected String getLastName() {
+		return lname;
+	} // getLastName()
+
+	protected void setLastName(String sLastName) {
+		lname = sLastName;
+	} // setLastName()
+
+	String uname;
+	protected String getUserName() {
+		return uname;
+	} // setUserName()
+
+	protected void setUserName(String sUserName) {
+		uname = sUserName;
+	} // setUserName()
+	
+	int weight;
+	protected int getWeight() {
+		return weight;
+	} // getWeight()
+
+	protected void setWeight(int nWeight) {
+		weight = nWeight;
+	} // setWeight()
+	
+	boolean male;
+	protected boolean isMale() {
+		return male;
+	} // isMale()
+
+	protected void setMale(boolean bIsMale) {
+		male = bIsMale;
+	} // setMale()
+
+	boolean bud;
+	public void setBuddy(String str){
+		bud = true;
+	}
+	
+	public boolean getBuddy(String str){
+		return bud;
+	}
+	
+	boolean grou;
+	public void setGroup(String str){
+		grou = true;
+	}
+	
+	public boolean getGroup(String str){
+		return grou;
+	}
 }
